af36b9c29ea552f935f320faf5855ed2e45ebfa1
subZero <cinkyal35@gmail.com>
2025-02-11 21:38:35 +0630

    make navbar and sidebar for admin dashboard UI

--- website/__init__.py
+++ website/__init__.py
@@ -1,15 +1,20 @@
-from flask import Flask
+from flask import Flask, send_from_directory
+import os
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'sub_zero'
+
+    @app.route('/node_modules/<path:filename>')
+    def serve_node_modules(filename):
+        return send_from_directory(os.path.join(app.root_path, '..', 'node_modules'), filename)
 
     from .views import views
     from .admin import admin
     from .auth import auth
 
     app.register_blueprint(views, prefix='/')
-    app.register_blueprint(admin, prefix='/')
+    app.register_blueprint(admin, prefix='/admin')
     app.register_blueprint(auth, prefix='/')
 
     return app
--- website/admin.py
+++ website/admin.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, render_template
 
 admin = Blueprint('admin',__name__)
-
+path = 'admin/'
 
 @admin.route('/dashboard')
 def dashboard():
-    return render_template('dashboard.html')
+    return render_template(path + 'dashboard.html')
