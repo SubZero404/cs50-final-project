3db538a78cdd7b7aff4cac8cefd38b4963f23517
sub zero <cinkyal35@gmail.com>
2025-02-12 23:51:05 +0630

    make models in models.py

--- website/__init__.py
+++ website/__init__.py
@@ -1,20 +1,36 @@
 from flask import Flask, send_from_directory
+from flask_sqlalchemy import SQLAlchemy
 import os
+
+db = SQLAlchemy()
+DB_NAME = 'database.db'
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'sub_zero'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+
+    db.init_app(app)
 
     @app.route('/node_modules/<path:filename>')
     def serve_node_modules(filename):
         return send_from_directory(os.path.join(app.root_path, '..', 'node_modules'), filename)
 
     from .views import views
-    from .admin import admin
+    from .admin import admin 
     from .auth import auth
 
     app.register_blueprint(views, prefix='/')
     app.register_blueprint(admin, prefix='/admin')
     app.register_blueprint(auth, prefix='/')
 
+    with app.app_context():
+        create_database()
+
     return app
+
+def create_database():
+    if not os.path.exists('website/' + DB_NAME):
+        from .models import Customer, Category, Product, Review, Cart, Order, OrderItem
+        db.create_all()
+        print('Database created!')
