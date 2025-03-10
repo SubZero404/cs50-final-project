662187ec79d941053883eca9123c33589e327a1b
sub zero <cinkyal35@gmail.com>
2025-03-10 23:45:03 +0630

    404 page

--- website/__init__.py
+++ website/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_from_directory 
+from flask import Flask, send_from_directory, render_template
 from flask_sqlalchemy import SQLAlchemy 
 from flask_login import LoginManager
 
@@ -13,6 +13,10 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
 
     db.init_app(app)
+
+    @app.errorhandler(404)
+    def pageNotFound(error):
+        return render_template('404.html')
 
     login_manager = LoginManager()
     login_manager.init_app(app)
